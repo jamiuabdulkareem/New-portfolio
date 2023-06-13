ac635c48b380f4954bee5dbf7058a0361ecf0284
jamiuabdulkareem <jamiuabdulkareem08@gmail.com>
2023-06-14 00:56:39 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 "use strict";
 const btnScrollTo = document.querySelector(".home-link");
-const ctaSection = document.querySelector("#cta");
-const ctaFname = document.querySelector("#fName");
-const ctaEmail = document.querySelector("#email");
-const ctaSubject = document.querySelector("#subject");
-const ctaMsg = document.querySelector("#msg");
 
-const myName = "Jamiu Williams AA.";
+const myName = "Jamiu Abidemi Abdulkareem.";
 let i = 0;
 // console.log(myName.length);
 
@@ -29,19 +24,3 @@ btnScrollTo.addEventListener("click", function (e) {
   e.preventDefault();
   ctaSection.scrollIntoView({ behavior: "smooth" });
 });
-
-// Contact form
-const sendEmail = function (e) {
-  e.preventDefault();
-  Email.send({
-    SecureToken: "23e6fa94-fcaa-4668-9311-067b86bd31e7",
-    To: "jamiuabdulkareem08@gmail.com",
-    From: ctaFname.value,
-    Subject: ctaSubject.value,
-    Body: ctaMsg.value,
-  }).then(function (message) {
-    alert("mail sent successfully");
-  });
-};
-
-document.querySelector(".ctaBtn").addEventListener("click", sendEmail);
